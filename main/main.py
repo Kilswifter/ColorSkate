8383b70400d638dbe9d11e47cafa2c9e735ef0a2
Kilswifter <Kilswifter@users.noreply.github.com>
2019-07-15 12:26:07 +0200

    ff

--- main/main.py
+++ main/main.py
@@ -1,8 +1,8 @@
 from ota_update.main.ota_updater import OTAUpdater
 
  def download_and_install_update_if_available():
-     o = OTAUpdater('url-to-your-github-project')
-     o.download_and_install_update_if_available('wifi-ssid', 'wifi-password')
+     o = OTAUpdater('https://github.com/Kilswifter/ColorSkate')
+     o.download_and_install_update_if_available('Avenue Belle Vue', 'Westfield19')
 
 
  def start():
@@ -10,6 +10,7 @@ from ota_update.main.ota_updater import OTAUpdater
      # from main.x import YourProject
      # project = YourProject()
      # ...
+     import colorskate.py
 
 
  def boot():
